6d705cddfa3261fcb541bede0995db03e1103a7e
wangqun6 <wangqun28>
2017-03-17 19:16:29 +0800

    change

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
-    console.log(req.ip);
-    next();
+app.use("/loadData", function (req, res, next) {
+    console.log(req.body);
+    res.send({code: 200});
 });
 
 app.use('/', index);
