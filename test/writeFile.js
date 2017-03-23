7b6a88178482dee05a6b747dc959eb11fd16aca0
king-king <niuyiwangqun@sohu.com>
2017-03-23 16:55:06 +0800

    改造，用stream代替写文件

--- test/writeFile.js
+++ test/writeFile.js
@@ -1,24 +1,22 @@
 /**
  * Created by wangqun6 on 2017/3/23.
  */
+// process.env.NODE_DEBUG = 'fs';
 
-
-var tool = require("../tool/tool");
-
+var t = require("../tool/tool");
+var fs = require("fs");
 //
 var t0 = Date.now();
 var src = "../logs/" + Date.now() + ".log";
-var task = [];
-tool.loop(10000, function (i) {
-    task.push(function (done) {
-        tool.write(src, {
-            in: i
-        }, setTimeout(done,0));
-    });
+
+var stream = fs.createWriteStream(src, {
+    flags: 'a'
 });
 
-tool.queue(task, function () {
-    console.log("write end: " + (Date.now() - t0));
+t.loop(1000000, function (i) {
+    stream.write(JSON.stringify({
+            index: i
+        }) + "\n");
 });
 
 
@@ -26,5 +24,4 @@ process.on("exit", function () {
     console.log(Date.now() - t0);
 });
 
-console.log("code end ");
 
