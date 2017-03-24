f319d3a1138ddedd3f2ee5c5b4a2b39561c50115
king-king <niuyiwangqun@sohu.com>
2017-03-24 17:28:16 +0800

    处理可写流close的问题

---
+++ test/parent.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wangqun6 on 2017/3/24.
+ */
+
+var os = require("os");
+var fork = require('child_process').fork;
+var t0 = Date.now();
+var t = require("../lib/tool");
+
+t.loop(1, function () {
+    var child = fork("./cluster-writeFile");
+    child.on("exit", function () {
+        // console.log("子进程退出了");
+    });
+});
+
+process.on("exit", function () {
+    console.log("父进程共耗时：" + (Date.now() - t0));
+});
