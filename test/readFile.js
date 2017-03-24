2ca4b4300b9f95e6675da6f9a3117552c62080f3
king-king <niuyiwangqun@sohu.com>
2017-03-24 18:16:03 +0800

    研究内容格式

--- test/readFile.js
+++ test/readFile.js
@@ -20,18 +20,29 @@ function timer(duration, func, callback) {
     d();
 }
 
-timer(1000 * 60, function () {
-    fs.readFile("../logs/out.log", {encoding: "utf-8"}, function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            var lineNumber = data.match(/\n/g);
-            lineNumber = (lineNumber && lineNumber.length) || 0;
-            console.log("当前文件写入了" + lineNumber + "行");
-        }
-    });
-}, function () {
-    console.log("结束读文件");
-}, 20);
+// timer(1000 * 60, function () {
+//     fs.readFile("../logs/out.log", {encoding: "utf-8"}, function (err, data) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             var lineNumber = data.match(/\n/g);
+//             lineNumber = (lineNumber && lineNumber.length) || 0;
+//             console.log("当前文件写入了" + lineNumber + "行");
+//         }
+//     });
+// }, function () {
+//     console.log("结束读文件");
+// }, 20);
+
+
+fs.readFile("e:/LOGS/out.log", {encoding: "utf-8"}, function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+        var d = JSON.parse(data.toString());
+        // console.log(d.index)
+    }
+});
 
 
