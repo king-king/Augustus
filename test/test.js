aebbdc93d34c50807909144785a378f8a0d17f8c
king-king <niuyiwangqun@sohu.com>
2017-03-27 18:13:52 +0800

    change change

--- test/test.js
+++ test/test.js
@@ -3,12 +3,13 @@
  */
 
 
-
+var time = require("../lib/time");
 var ump = require("../lib/ump_log_writer");
 var f = ump.tp();
 var t0 = Date.now();
+var t = time.getUMPtime();
 for (var i = 0; i < 1000000; i++) {
-    f.write("zs.jd.com", "123");
+    f.write("zs.jd.com", "123", t);
 }
 console.log("run :" + (Date.now() - t0) + "ms");
 
