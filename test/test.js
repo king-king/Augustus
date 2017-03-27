c07b975de4de4d49716616bdf132c10879a4a766
king-king <niuyiwangqun@sohu.com>
2017-03-27 16:33:57 +0800

    change

--- test/test.js
+++ test/test.js
@@ -2,41 +2,19 @@
  * Created by wangqun6 on 2017/3/27.
  */
 
-// var os = require("os");
-// console.log(JSON.stringify(os.userInfo()));
-function timer(func, delay) {
-    var id;
 
-    function d() {
-        clearTimeout(id);
-        func();
-        id = setTimeout(d, delay);
+
+function test() {
+    var d = Date.now();
+    for (var i = 0; i < 3000000; i++) {
+        // var str = i.toString();
+        var str = i + "";
     }
-
-    return {
-        start: d,
-        stop: function () {
-            clearTimeout(id);
-        }
-    };
+    console.log(Date.now() - d);
 }
-
-var i = 0;
-var t = timer(function () {
-    var str = "";
-    for (var n = 0; n < 10; n++) {
-        str += n;
-    }
-    str = null;
-    console.log(i++)
-}, 0);
-
-try {
-    t.start();
-} catch (e) {
-    console.log(e);
+var count = 50;
+var t1 = Date.now();
+for (var i = 0; i < count; i++) {
+    test();
 }
-
-process.on('exit', function (code) {
-    console.log("About to exit with code:" + code);
-});
+console.log("end :" + (Date.now() - t1));
